b60b0afcb90a5c0d7fdfefdd4cb90675e76a5b62
Eric Kivel <ericvkivel1@gmail.com>
2023-09-24 19:27:55 -0300

    fix: newton term outside for loop

--- src/main.c
+++ src/main.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <sys/time.h>
 
 #include "../include/interpolation.h"
-//#include <likwid.h>
+// #include <likwid.h>
 
-int main(char* args, int argc) {
+int main(int argc, char **args) {
   // Arrange
-  
+
   double xe = atof(args[1]);
   int size;
   scanf("%d", &size);
@@ -17,22 +18,22 @@ int main(char* args, int argc) {
 
   readPairs(pairs, size);
 
-  //LIKWID_MARKER_INIT;
+  // LIKWID_MARKER_INIT;
 
   // Calculate
-  //LIKWID_MARKER_START("Sol_1");
+  // LIKWID_MARKER_START("Sol_1");
   gettimeofday(&start1, NULL);
-  double newtonSolution = newtonInterpolation(pairs, size, xe);
-  gettimeofday(&end1, NULL);
-  //LIKWID_MARKER_STOP("Sol_1");
-
-  //LIKWID_MARKER_START("Sol_2");
-  gettimeofday(&start2, NULL);
   double lagrangeSolution = lagrangeInterpolation(pairs, size, xe);
-  gettimeofday(&end2, NULL);
-  //LIKWID_MARKER_STOP("Sol_2");
+  gettimeofday(&end1, NULL);
+  // LIKWID_MARKER_STOP("Sol_1");
 
- //LIKWID_MARKER_CLOSE;
+  // LIKWID_MARKER_START("Sol_2");
+  gettimeofday(&start2, NULL);
+  double newtonSolution = newtonInterpolation(pairs, size, xe);
+  gettimeofday(&end2, NULL);
+  // LIKWID_MARKER_STOP("Sol_2");
+
+  // LIKWID_MARKER_CLOSE;
 
   // Show
   printf("\n");
@@ -41,8 +42,8 @@ int main(char* args, int argc) {
   printf("\n");
 
   printf("\n");
-  printf("Newton: %lf\n", lagrangeSolution);
-    printTime(start2, end2);
+  printf("Newton: %lf\n", newtonSolution);
+  printTime(start2, end2);
   printf("\n");
 
   // Free
--- src/solve.c
+++ src/solve.c
@@ -6,10 +6,10 @@
 double lagrangeInterpolation(struct Pair *pairs, int size, double xe) {
   double result = 0;
 
-  for(int i = 0; i < size; i++) {
+  for (int i = 0; i < size; i++) {
     double term = pairs[i].second;
-    for(int j = 0; j < size; j++) {
-      if(i != j) {
+    for (int j = 0; j < size; j++) {
+      if (i != j) {
         term *= (xe - pairs[j].first) / (pairs[i].first - pairs[j].first);
       }
     }
@@ -24,19 +24,21 @@ double newtonInterpolation(struct Pair *pairs, int size, double xe) {
   double result = 0;
 
   // Calculate divided differences - DP
-  for(int i = 0; i < size; i++) {
+  for (int i = 0; i < size; i++) {
     dividedDifferences[i][0] = pairs[i].second;
   }
 
-  for(int j = 1; j < size; j++) {
-    for(int i = 0; i < size - j; i++) {
-      dividedDifferences[i][j] = (dividedDifferences[i+1][j-1] - dividedDifferences[i][j-1])/(pairs[i+j].first - pairs[i].first);
+  for (int j = 1; j < size; j++) {
+    for (int i = 0; i < size - j; i++) {
+      dividedDifferences[i][j] =
+          (dividedDifferences[i + 1][j - 1] - dividedDifferences[i][j - 1]) /
+          (pairs[i + j].first - pairs[i].first);
     }
   }
 
-  for(int i = 0; i < size; i++) {
-    double term = 1.0;
-    result += term*dividedDifferences[0][i];
+  double term = 1.0;
+  for (int i = 0; i < size; i++) {
+    result += term * dividedDifferences[0][i];
     term *= (xe - pairs[i].first);
   }
 
